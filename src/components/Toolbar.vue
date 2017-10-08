eba9c56f8689042a19fc9aad9ba963db88a5ae35
Adel Mahjoub <dragonhunter250@gmail.com>
2017-10-08 22:11:23 +0200

    bigger add recipe  button

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -3,21 +3,21 @@
     <!-- left side of the toolbar -->
     <div class="level-left">
       <div class="level-item">
-        
-      </div>
-    </div>
-    <!-- right side of the toolbar -->
-    <div class="level-right">
-      <div class="level-item">
         <router-link
         v-show="notInEditMode"
         :to="{name: 'recipes.edit', params: {id: 'new'}}"
-        class="button is-outlined is-primary is-small">
+        class="button is-outlined is-primary">
           <span class="icon is-small">
             <i class="fa fa-plus"></i>
           </span>
           <span>New</span>
         </router-link>
+      </div>
+    </div>
+    <!-- right side of the toolbar -->
+    <div class="level-right">
+      <div class="level-item">
+        
       </div>
     </div>
   </div>
